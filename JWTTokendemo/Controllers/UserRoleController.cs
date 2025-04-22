6b7bd2e1985bf35e5a1db808144c65b84457a553
Roshan <dell@ROSHAN>
2025-04-22 10:24:11 +0530

    UserRole Controller addedd

---
+++ JWTTokendemo/Controllers/UserRoleController.cs
@@ -0,0 +1,26 @@
+﻿using JWTTokendemo.Data.Entities;
+using JWTTokendemo.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTTokendemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserRoleController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public UserRoleController(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+        [HttpPost("UserRole")]
+        public IActionResult AddUserRole(UserRole userRole)
+        {
+            _context.UserRoles.Add(userRole);
+            _context.SaveChanges();
+            return Ok("Added User Role succesfully!");
+        }
+    }
+}
