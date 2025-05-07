d0aec64a867f26a8106e1658e38ec458c631f368
Roshan <dell@ROSHAN>
2025-05-07 09:24:44 +0530

    currency class

---
+++ JWTTokendemo/Controllers/CurrencyController.cs
@@ -0,0 +1,31 @@
+﻿using JWTTokendemo.Data.Entities;
+using JWTTokendemo.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTTokendemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public CurrencyController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("AddCurrency")]
+        public IActionResult AddCurrency(Currency currency)
+        {
+            _context.Currencies.Add(currency);
+            _context.SaveChanges();
+            return Ok("Add Currency succesfully!");
+        }
+        [HttpGet("GetAllCurrency")]
+        public IActionResult Currencies()
+        {
+            return Ok(_context.Currencies.ToList());
+        }
+    }
+}
---
+++ JWTTokendemo/Data/Entities/Currency.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTTokendemo.Data.Entities
+{
+    public class Currency
+    {
+        public int Id { get; set; }
+        public required string CurrencyName { get; set; }
+    }
+}
